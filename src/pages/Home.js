4366e6a65c5bdef6b7a344598be1bbe286a4a957
Arushi <khethavatharushi@gmail.com>
2026-04-24 12:09:01 +0530

    Add profile image to Home page
    Added profile image to the hero section.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,6 +13,7 @@ function Home() {
       <section className="hero">
         <div className="hero-bg-text">ARUSHI</div>
         <div className="hero-content">
+          <img src="/portfolio/profile.jpg" alt="Arushi Khethavath" className="hero-img" />
           <p className="hero-pre">Computer Science</p>
           <h1 className="hero-name">
             Arushi<br />
